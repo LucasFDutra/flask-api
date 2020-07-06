6d038198e53ef8a00c2fd45f6a166d4a0c29f739
LucasFDutra <lucasfelipedutra@gmail.com>
2020-07-05 22:46:29 -0300

    mudança no teste

--- src/controllers/UserController_test.py
+++ src/controllers/UserController_test.py
@@ -4,4 +4,4 @@ from src.controllers.UserController import UserController
 def test_soma():
     user_controller = UserController()
     assert user_controller.soma(1, 2) == 3
-    assert user_controller.soma('aba', 'caxi') == 'abacaxi'
+    assert user_controller.soma('aba', 'cate') == 'abacate'
