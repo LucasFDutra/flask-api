2e7b234c10468af2d555b0076fd732038a889a5b
LucasFDutra <lucasfelipedutra@gmail.com>
2020-07-05 23:17:51 -0300

    teste

--- src/controllers/UserController_test.py
+++ src/controllers/UserController_test.py
@@ -5,3 +5,4 @@ def test_soma():
     user_controller = UserController()
     assert user_controller.soma(1, 2) == 3
     assert user_controller.soma('aba', 'caxi') == 'abacaxi'
+    assert user_controller.soma('aba', 'cate') == 'abacate'
